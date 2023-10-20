e277b33c807c45051806d6c09bef1a588482cea9
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-10-20 12:09:38 +0545

    Adding header scroll state in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ const $navbar = document.querySelector("[data-navbar]");
 const $navToggler = document.querySelector("[data-nav-toggler   ]");
 
 $navToggler.addEventListener('click', () => $navbar.classList.toggle("active"));
+
+
+// Header scroll state
+
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
