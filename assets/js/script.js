25044579249edcc7172dac1f00fdad6877afc04d
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-10-30 15:46:55 +0545

    Debug in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@
 
 // Navbar toggle in mobile
 
-const $navbar = document.querySelector("[data-navbar]");
-const $navToggler = document.querySelector("[data-nav-toggler   ]");
+const $navbar = document.querySelector('[data-navbar]');
+const $navToggler = document.querySelector('[data-nav-toggler   ]');
 
-$navToggler.addEventListener('click', () => $navbar.classList.toggle("active"));
-
+$navToggler.addEventListener('click', () => $navbar.classList.toggle('active'));
 
 // Header scroll state
 
-const $header = document.querySelector("[data-header]");
+const $header = document.querySelector('[data-header]');
 
-window.addEventListener("scroll", e => {
-    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+window.addEventListener('scroll', e => {
+  $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');
 });
